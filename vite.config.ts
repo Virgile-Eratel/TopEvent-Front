23b025f8573dd0fd47fa64489d448a8d097b519d
Virgile Marty <eratel@MacBookPro.local>
2025-10-16 12:04:38 +0200

    Ajout shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 
 // https://vite.dev/config/
@@ -13,6 +14,11 @@ export default defineConfig({
         strictPort: true,
         hmr: {
             clientPort: 5173,
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
         },
     },
 })
